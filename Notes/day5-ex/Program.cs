8d13e541a5ef9f41c2ba45a59b05df72f9a638d9
Jonah <Jonah@Bullet-Cluster.local>
2020-02-28 09:15:30 -0600

    initial acronym creator code upload

---
+++ Notes/day5-ex/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace acronymcreator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Input a term/phrase.");
+            string phrase = getPhrase();
+        }
+
+        public string getPhrase()
+        {
+            return Console.Readline();
+        }
+
+        public void makeAcronym()
+        {
+
+        }
+    }
+}
