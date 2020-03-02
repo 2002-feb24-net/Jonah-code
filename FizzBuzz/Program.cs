2ac13435d951dce6b1fbf5f4a68a048761f2d14b
Jonah <Jonah@Bullet-Cluster.local>
2020-03-02 13:55:54 -0600

    finished in-class 'FizzBuzz' assignment

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        // There are 267 Fizzes, 134 Buzzes, and 66 FizzBuzzes from 1 to 1000
+        static void Main(string[] args)
+        {
+            int fCount = 0;
+            int bCount = 0;
+            int fbCount = 0;
+            for (int i = 1; i < 1001; i++) {
+                if(i%3 == 0 && i%5 != 0) 
+                {
+                    System.Console.Write("Fizz ");
+                    fCount++;
+                }
+                else if(i%5 == 0 && i%3 != 0)
+                {
+                    System.Console.Write("Buzz ");
+                    bCount++;
+                }
+                else if(i%3 == 0 && i%5 == 0)
+                {
+                    System.Console.Write("FizzBuzz ");
+                    fbCount++;
+                }
+                else
+                {
+                    System.Console.Write($"{i} ");
+                }
+            }
+            System.Console.WriteLine($"\nFizz count: {fCount}\nBuzz count: {bCount}\nFizzBuzz count: {fbCount}");
+        }
+    }
+}
